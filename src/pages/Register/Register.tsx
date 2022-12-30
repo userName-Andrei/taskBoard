341cfc720f27075ff6dce3573411d05178b047e4
Andrei <andrei_zhigachev@bk.ru>
2022-12-30 12:12:57 +0300

    added spinner when loading=true

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import SignUp from '../../components/SignUp/SignUp';
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../../hooks/useAuth';
+import Spinner from '../../components/Spinner';
+import { useAppSelector } from '../../hooks/useAppSelector';
 
 const Register = () => {
 
     const {isAuth} = useAuth();
+    const {loading} = useAppSelector(state => state.auth);
 
     return isAuth ? (
         <Navigate to="/" replace={true} />
@@ -13,11 +16,16 @@ const Register = () => {
         <div className="auth">
             <div className='auth__wrapper card'>
 
-                <div className="auth__top card__top card__top_center">
-                    <h1 className='card__title'>Registration</h1>
-                </div>
+                {loading ?
+                <Spinner /> :
+                <>
+                    <div className="auth__top card__top card__top_center">
+                        <h1 className='card__title'>Registration</h1>
+                    </div>
 
-                <SignUp />
+                    <SignUp />
+                </> }
+
             </div>
         </div>
     );
